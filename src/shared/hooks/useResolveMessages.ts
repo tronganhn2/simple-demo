d4ba4e5e65c1ffa01d1e8bae7089861ef7a86d79
anhtrongnguyen <annguyen@agreementexpress.com>
2022-07-28 00:00:31 +0700

    update jsdoc

--- src/shared/hooks/useResolveMessages.ts
+++ src/shared/hooks/useResolveMessages.ts
@@ -1,5 +1,9 @@
 import { MessageType } from '../../utils/message.types';
 
+/**
+* Normally, this custom hook is for resolve messages to different languages.
+* For now, it's just a dummy hook for demo-ing purposes
+*/
 export const useResolvedMessages = (messages: MessageType): MessageType => {
 	return messages
 };
